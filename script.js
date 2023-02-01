8368abcb61351068e2830d718b58f6656b44ae06
Fernando Bustillos <bustillos007@gmail.com>
2023-01-31 22:34:45 -0500

    finished project

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("testing");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+//Promp to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.onplay();
+    };
+  } catch (error) {
+    //catch error here
+    console.log("whoops, error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disable = true;
+  //Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  //Reset Button
+  button.disable = false;
+});
+
+//On Load
+selectMediaStream();
